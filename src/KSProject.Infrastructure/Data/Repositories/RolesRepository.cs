46b618adac30032e75f5b43b418f0b5afdf6d168
Kamran Sadin <mrsadin@gmail.Com>
2025-08-27 06:46:10 +0330

    Get Role Permissions By Role Id Endpoint

--- src/KSProject.Infrastructure/Data/Repositories/RolesRepository.cs
+++ src/KSProject.Infrastructure/Data/Repositories/RolesRepository.cs
@@ -6,34 +6,42 @@ namespace KSProject.Infrastructure.Data.Repositories;
 
 public sealed class RolesRepository : GenericRepository<Role>, IRolesRepository
 {
-    private readonly DbSet<Role> _roles;
-    public RolesRepository(DbContext context) : base(context)
-    {
-        _roles = context.Set<Role>();
-    }
+	private readonly DbSet<Role> _roles;
+	public RolesRepository(DbContext context) : base(context)
+	{
+		_roles = context.Set<Role>();
+	}
 
-    public async Task<Role?> GetByRoleName(string roleName,
-        CancellationToken cancellationToken = default)
-    {
-        return await _roles
-            .FirstOrDefaultAsync(x => x.Name.ToLower() == roleName,
-                cancellationToken);
-    }
+	public async Task<Role?> GetByRoleName(string roleName,
+		CancellationToken cancellationToken = default)
+	{
+		return await _roles
+			.FirstOrDefaultAsync(x => x.Name.ToLower() == roleName,
+				cancellationToken);
+	}
 
-    public async Task<Role?> GetRoleByIdIncludingUsersAsync(Guid id,
-        CancellationToken cancellationToken = default)
-    {
-        return await _roles
-            .Include(r => r.Users)
-            .FirstOrDefaultAsync(r => r.Id == id,
-                cancellationToken);
-    }
+	public async Task<Role?> GetRoleByIdIncludingUsersAsync(Guid id,
+		CancellationToken cancellationToken = default)
+	{
+		return await _roles
+			.Include(r => r.Users)
+			.FirstOrDefaultAsync(r => r.Id == id,
+				cancellationToken);
+	}
 
-    public async Task<List<Role>> GetRolesByIdsAsync(IEnumerable<Guid> ids,
-        CancellationToken cancellationToken = default)
-    {
-        return await _roles
-            .Where(r => ids.Contains(r.Id))
-            .ToListAsync(cancellationToken);
-    }
+	public async Task<List<Role>> GetRolesByIdsAsync(IEnumerable<Guid> ids,
+		CancellationToken cancellationToken = default)
+	{
+		return await _roles
+			.Where(r => ids.Contains(r.Id))
+			.ToListAsync(cancellationToken);
+	}
+
+	public async Task<Role?> GetRoleAndPermissionsAsync(Guid id,
+		CancellationToken cancellationToken = default)
+	{
+		return await _roles
+			.Include(r => r.Permissions)
+			.FirstOrDefaultAsync(r => r.Id == id);
+	}
 }
--- src/KSProject.Presentation/Controllers/AdminControllers/RolesController.cs
+++ src/KSProject.Presentation/Controllers/AdminControllers/RolesController.cs
@@ -5,6 +5,7 @@ using KSProject.Application.Roles.DeleteRole;
 using KSProject.Application.Roles.GetAllRoles;
 using KSProject.Application.Roles.GetPaginatedRoles;
 using KSProject.Application.Roles.GetRoleById;
+using KSProject.Application.Roles.GetRolePermissionsByRoleId;
 using KSProject.Application.Roles.UpdateRole;
 using KSProject.Presentation.Attributes;
 using KSProject.Presentation.BaseControllers;
@@ -16,111 +17,133 @@ namespace KSProject.Presentation.Controllers.AdminControllers;
 
 public sealed class RolesController(ISender sender) : BaseController(sender)
 {
-    [HttpGet]
-    [Route(Routes.Roles_Admin.GetPagedRoles)]
-    [ProducesResponseType(StatusCodes.Status200OK)]
-    [ProducesResponseType(StatusCodes.Status400BadRequest)]
-    [Produces("application/json")]
-    public async Task<ActionResult<PaginatedList<RolesListItemResponse>>> GetAsync(
-        [FromQuery] GetPaginatedRolesRequest options,
-        CancellationToken cancellationToken = default)
-    {
-        GetPaginatedRolesQuery query = new(options);
-        
-        PaginatedList<RolesListItemResponse> result = await Sender.Send(query,
-            cancellationToken);
+	[HttpGet]
+	[Permission("GetPagedRoles")]
+	[Route(Routes.Roles_Admin.GetPagedRoles)]
+	[ProducesResponseType(StatusCodes.Status200OK)]
+	[ProducesResponseType(StatusCodes.Status400BadRequest)]
+	[Produces("application/json")]
+	public async Task<ActionResult<PaginatedList<RolesListItemResponse>>> GetAsync(
+		[FromQuery] GetPaginatedRolesRequest options,
+		CancellationToken cancellationToken = default)
+	{
+		GetPaginatedRolesQuery query = new(options);
 
-        return Ok(result);
-    }
-    
-    [HttpGet]
-    [Route(Routes.Roles_Admin.GetAllRoles)]
-    [ProducesResponseType(StatusCodes.Status200OK)]
-    [ProducesResponseType(StatusCodes.Status400BadRequest)]
-    [Produces("application/json")]
-    public async Task<ActionResult<List<GetAllRolesResponse>>> GetAsync(
-        [FromQuery] GetAllRolesRequest request,
-        CancellationToken cancellationToken = default)
-    {
-        GetAllRolesQuery query = new(request);
-        
-        List<GetAllRolesResponse> result = await Sender.Send(query,
-            cancellationToken);
+		PaginatedList<RolesListItemResponse> result = await Sender.Send(query,
+			cancellationToken);
 
-        return Ok(result);
-    }
+		return Ok(result);
+	}
 
-    [HttpGet]
-    [Permission("GetRoleById")]
-    [Route(Routes.Roles_Admin.GetRoleById)]
-    [Produces(typeof(RoleItemResponse))]
-    [ProducesResponseType(StatusCodes.Status200OK)]
-    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-    public async Task<ActionResult<RoleItemResponse>> GetAsync(
-        [FromRoute] GetRoleByIdRequest payload,
-        CancellationToken cancellationToken = default)
-    {
-        GetRoleByIdQuery query = new(payload);
+	[HttpGet]
+	[Permission("GetAllRoles")]
+	[Route(Routes.Roles_Admin.GetAllRoles)]
+	[ProducesResponseType(StatusCodes.Status200OK)]
+	[ProducesResponseType(StatusCodes.Status400BadRequest)]
+	[Produces("application/json")]
+	public async Task<ActionResult<List<GetAllRolesResponse>>> GetAsync(
+		[FromQuery] GetAllRolesRequest request,
+		CancellationToken cancellationToken = default)
+	{
+		GetAllRolesQuery query = new(request);
 
-        RoleItemResponse result = await Sender.Send(query, cancellationToken);
+		List<GetAllRolesResponse> result = await Sender.Send(query,
+			cancellationToken);
 
-        return Ok(result);
-    }
+		return Ok(result);
+	}
 
-    [HttpPut]
-    [Route(Routes.Roles_Admin.UpdateRole)]
-    [Produces(typeof(RoleUpdateResponse))]
-    [ProducesResponseType(StatusCodes.Status200OK)]
-    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-    [ProducesResponseType(StatusCodes.Status400BadRequest)]
-    public async Task<ActionResult<RoleUpdateResponse>> PutAsync(
-        Guid id,
-        [FromBody] RoleUpdateRequest payload,
-        CancellationToken cancellationToken = default)
-    {
-        if (id != payload.Id)
-            return BadRequest("The record could not be found.");
-        
-        RoleUpdateCommand command = new(payload);
+	[HttpGet]
+	[Permission("GetRoleById")]
+	[Route(Routes.Roles_Admin.GetRoleById)]
+	[Produces(typeof(RoleItemResponse))]
+	[ProducesResponseType(StatusCodes.Status200OK)]
+	[ProducesResponseType(StatusCodes.Status500InternalServerError)]
+	public async Task<ActionResult<RoleItemResponse>> GetAsync(
+		[FromRoute] GetRoleByIdRequest payload,
+		CancellationToken cancellationToken = default)
+	{
+		GetRoleByIdQuery query = new(payload);
 
-        RoleUpdateResponse result = await Sender.Send(command, cancellationToken);
+		RoleItemResponse result = await Sender.Send(query, cancellationToken);
 
-        return Ok(result);
-    }
-    
-    [HttpPost]
-    [Route(Routes.Roles_Admin.CreateRole)]
-    [ProducesResponseType(StatusCodes.Status200OK)]
-    [ProducesResponseType(StatusCodes.Status400BadRequest)]
-    [ProducesResponseType(typeof(CreateRoleResponse),
-        StatusCodes.Status200OK)]
-    public async Task<ActionResult<CreateRoleResponse>> PostAsync(
-        [FromBody] CreateRoleRequest request,
-        CancellationToken cancellationToken = default)
-    {
-        CreateRoleCommand command = new(request);
+		return Ok(result);
+	}
 
-        CreateRoleResponse result = await Sender.Send(command,
-            cancellationToken);
+	[HttpGet]
+	[Permission("GetRolePermissions")]
+	[Route(Routes.Roles_Admin.Role_Permissions.GetRolePermissions)]
+	[Produces(typeof(RolePermissionsResponse))]
+	[ProducesResponseType(StatusCodes.Status200OK)]
+	[ProducesResponseType(StatusCodes.Status500InternalServerError)]
+	public async Task<ActionResult<RolePermissionsResponse>> GetRolePermissionsAsync(
+		[FromRoute] GetRolePermissionsByRoleIdRequest payload,
+		CancellationToken cancellationToken = default)
+	{
+		GetRolePermissionsQuery query = new(payload);
 
-        return Ok(result);
-    }
-    
-    [HttpDelete]
-    [Route(Routes.Roles_Admin.DeleteRole)]
-    [ProducesResponseType(StatusCodes.Status200OK)]
-    [ProducesResponseType(StatusCodes.Status400BadRequest)]
-    [ProducesResponseType(typeof(DeleteRoleResponse),
-        StatusCodes.Status200OK)]
-    public async Task<ActionResult<DeleteRoleResponse>> DeleteAsync(
-        [FromRoute] DeleteRoleRequest request,
-        CancellationToken cancellationToken = default)
-    {
-        DeleteRoleCommand command = new(request);
+		RolePermissionsResponse result = await Sender.Send(query, cancellationToken);
 
-        DeleteRoleResponse result = await Sender.Send(command,
-            cancellationToken);
+		return Ok(result);
+	}
 
-        return Ok(result);
-    }
+	[HttpPut]
+	[Permission("UpdateRole")]
+	[Route(Routes.Roles_Admin.UpdateRole)]
+	[Produces(typeof(RoleUpdateResponse))]
+	[ProducesResponseType(StatusCodes.Status200OK)]
+	[ProducesResponseType(StatusCodes.Status500InternalServerError)]
+	[ProducesResponseType(StatusCodes.Status400BadRequest)]
+	public async Task<ActionResult<RoleUpdateResponse>> PutAsync(
+		Guid id,
+		[FromBody] RoleUpdateRequest payload,
+		CancellationToken cancellationToken = default)
+	{
+		if (id != payload.Id)
+			return BadRequest("The record could not be found.");
+
+		RoleUpdateCommand command = new(payload);
+
+		RoleUpdateResponse result = await Sender.Send(command, cancellationToken);
+
+		return Ok(result);
+	}
+
+	[HttpPost]
+	[Permission("CreateRole")]
+	[Route(Routes.Roles_Admin.CreateRole)]
+	[ProducesResponseType(StatusCodes.Status200OK)]
+	[ProducesResponseType(StatusCodes.Status400BadRequest)]
+	[ProducesResponseType(typeof(CreateRoleResponse),
+		StatusCodes.Status200OK)]
+	public async Task<ActionResult<CreateRoleResponse>> PostAsync(
+		[FromBody] CreateRoleRequest request,
+		CancellationToken cancellationToken = default)
+	{
+		CreateRoleCommand command = new(request);
+
+		CreateRoleResponse result = await Sender.Send(command,
+			cancellationToken);
+
+		return Ok(result);
+	}
+
+	[HttpDelete]
+	[Permission("DeleteRole")]
+	[Route(Routes.Roles_Admin.DeleteRole)]
+	[ProducesResponseType(StatusCodes.Status200OK)]
+	[ProducesResponseType(StatusCodes.Status400BadRequest)]
+	[ProducesResponseType(typeof(DeleteRoleResponse),
+		StatusCodes.Status200OK)]
+	public async Task<ActionResult<DeleteRoleResponse>> DeleteAsync(
+		[FromRoute] DeleteRoleRequest request,
+		CancellationToken cancellationToken = default)
+	{
+		DeleteRoleCommand command = new(request);
+
+		DeleteRoleResponse result = await Sender.Send(command,
+			cancellationToken);
+
+		return Ok(result);
+	}
 }
